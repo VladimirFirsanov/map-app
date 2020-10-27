d941a28827566a746076a87893c87fe8f668e1f4
Владимир Фирсанов <vladimir.firsanov@rtlabs.ru>
2020-10-27 23:19:22 +0300

    added position synchronizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,52 @@
-import { Component } from '@angular/core';
-import { AcNotification } from 'angular-cesium';
-import { Observable } from 'rxjs';
+import { AfterViewInit, Component } from '@angular/core';
+import { MapsManagerService, SceneMode, ViewerConfiguration } from 'angular-cesium';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ViewerConfiguration],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'map-app';
 
-  planes$: Observable<AcNotification>;
-  showTracks  = true;
+  maps = [
+    {
+      id: 'map-first',
+      sceneMode: SceneMode.PERFORMANCE_SCENE2D,
+      containerId : 'first-map-container'
+    },
+    {
+      id: 'map-second',
+      sceneMode: SceneMode.PERFORMANCE_SCENE2D,
+      containerId : 'second-map-container'
+    },
+    {
+      id: 'map-third',
+      sceneMode: SceneMode.PERFORMANCE_SCENE2D,
+      containerId : 'third-map-container'
+    },
+    {
+      id: 'map-forth',
+      sceneMode: SceneMode.PERFORMANCE_SCENE2D,
+      containerId : 'forth-map-container'
+    }
+  ];
 
+  constructor(
+    private viewerConf: ViewerConfiguration,
+    private mapsManagerService: MapsManagerService,
+  ) {
+
+    viewerConf.viewerOptions = [];
+  }
+
+  ngAfterViewInit(): void {
+    this.mapsManagerService.sync2DMapsCameras([
+      {id: 'map-first'},
+      {id: 'map-second'},
+      {id: 'map-third'},
+      {id: 'map-forth'},
+    ]);
+  }
 }
