deb43c352f20d38380294bc481d1830a829292b6
Владимир Фирсанов <vladimir.firsanov@rtlabs.ru>
2020-10-27 22:04:43 +0300

    init app with four map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AcNotification } from 'angular-cesium';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'map-app';
+
+  planes$: Observable<AcNotification>;
+  showTracks  = true;
+
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as Cesium from 'cesium';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+window['CESIUM_BASE_URL'] = '/assets/cesium';
